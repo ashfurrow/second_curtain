74dd2de33fb3a96f48d5ce602ffae04217f00b10
Eloy Durán <eloy.de.enige@gmail.com>
2015-05-12 17:21:31 +0200

    Add support for automatically using CircleCI build number as prefix.

--- lib/second_curtain/web_preview.rb
+++ lib/second_curtain/web_preview.rb
@@ -11,6 +11,6 @@ class WebPreview
     lib_path = File.expand_path(File.dirname(__FILE__))
     template = File.read(lib_path + "/template.mustache.html")
     
-    Mustache.render(template, :uploads => @uploads, :travis_id =>  ENV['TRAVIS_JOB_ID'])
+    Mustache.render(template, :uploads => @uploads, :travis_id =>  ENV['TRAVIS_JOB_ID'], :circle_ci_id => ENV['CIRCLE_BUILD_NUM'])
   end
 end
